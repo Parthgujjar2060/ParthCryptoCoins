c5b78f10f2ba690e155a321d04d848a77dae90d0
Parthgujjar2060 <parthgujjar2003@gmail.com>
2023-12-20 14:34:09 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Header from "./component/Header"
+import CoinDetail from "./component/CoinDetail"
+import Exchanges from "./component/Exchanges"
+import Home from "./component/Home"
+import Coins from "./component/Coins"
+
+
+function App() {
+  return (
+      <Router>
+          <Header/>
+       <Routes>
+         <Route path="/" element = {<Home/>} />
+         <Route path="/Coins" element = {<Coins/>} />
+         <Route path="/Header" element = {<Header/>} />
+         <Route path="/Exchange" element = {<Exchanges/>} />
+         <Route path="/Coin/:id" element = {<CoinDetail/>} />                
+        </Routes>
+      </Router>  
+     
+  );
+}       
+
+export default App;
