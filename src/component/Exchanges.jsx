0f8ee1749533c6ffa54b9840496b1842e49f8cd7
Parthgujjar2060 <parthgujjar2003@gmail.com>
2023-12-21 19:09:55 -0500

    Here i can see the images and other information!!!

--- src/component/Exchanges.jsx
+++ src/component/Exchanges.jsx
@@ -1,20 +1,32 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { Container, HStack } from '@chakra-ui/react';
-import Loader from './Loader';  
+import { Container, HStack, VStack, Image } from '@chakra-ui/react';
+import Loader from './Loader';
 import { server } from '../index';
 
+const ExchangeItem = ({ name, image, trust_score, url }) => (
+  <div>
+    <a href={url} target='_blank'>
+      <p>Name: {name}</p>
+      <p>Image: {image}</p>
+      <p>Trust Score: {trust_score}</p>
+      <VStack>
+        <Image src={image} w="10" h="10" alt={name} />
+      </VStack>
+    </a>
+  </div>
+);
+
 const Exchanges = () => {
-  const [exchange, setExchange] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchExchanges = async () => {
       try {
         const { data } = await axios.get(`${server}/exchanges?page=3`);
-
         console.log(data);
-        setExchange(data);
+        setExchanges(data);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching exchanges:', error);
@@ -25,20 +37,27 @@ const Exchanges = () => {
     fetchExchanges();
   }, []);
 
-  return (  
+  return (
     <Container maxW="container.x1">
-      {loading ? 
+      {loading ? (
         <Loader />
-       : (
-        <HStack wrap={"wrap"}>
-          {exchange.map((item) => (
-         
-            <div key={item.id}>{item.name}</div>
+      ) : (
+        <HStack wrap="wrap">
+          {exchanges.map((item) => (
+            <ExchangeItem
+              key={item.id}
+              name={item.name}
+              image={item.image}
+              trust_score={item.trust_score}
+              url={item.url}
+            />
           ))}
         </HStack>
       )}
     </Container>
   );
 };
+
+
 
 export default Exchanges;
